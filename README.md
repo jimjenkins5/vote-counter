4fe818fe676c0794b67cd50e804230d5cbe0d514
Jim Jenkins <jimjenkins5@gmail.com>
2020-02-21 15:43:44 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Borda Vote Counter
+
+## What?
+
+This small script takes a json file of ballets and determines the winner using the Borda method.
+
+## How do I use it?i
+
+```bash
+npx borda-vote-counter /path/to/votes.json
+```
+
+Format of votes.json
+
+```json
+[
+  [ "OB", "SL" ],
+  [ "SL", "CL", "OB", "SB" ],
+  [ "OB", "SL", "SB", "CL", "TR" ],
+  [ "SL", "SB", "TR", "CL", "OB" ],
+  [ "OB" ],
+  [ "OB", "SB", "SL" ],
+  [ "TR", "CL", "SL", "OB" ],
+  [ "SB", "CL", "OB" ]
+]
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs'),
+      util = require('util'),
+      readFile = util.promisify(fs.readFile),
+      opts = [ 'SL', 'OB', 'TR', 'CL', 'SB' ],
+      filePath = process.argv.length > 2 ? process.argv[2] : null;
+
+if (!filePath) {
+   console.error('You must pass a filePath as an argument!');
+}
+
+(async function() {
+   const ballots = await readFile(filePath, 'utf8')
+      .then((json) => { return JSON.parse(json); });
+
+   // Get the value of each option in each ballot
+   const processed = ballots.map((ballot) => {
+      return ballot.reduce((memo, vote, index) => {
+         memo[vote] = opts.length - index;
+         return memo;
+      }, {});
+   });
+
+   // Add up the value of each option globally
+   const counted = opts.reduce((memo, opt) => {
+      const value = processed.map((o) => { return o[opt] })
+         .reduce((count, val) => {
+            if (!val) {
+               return count;
+            }
+            return count + val;
+         }, 0)
+
+      memo.push({ option: opt, value: value });
+      return memo;
+   }, []);
+
+
+   const winner = counted.reduce((win, current) => {
+      if (current.value > win.value) {
+         return current;
+      }
+      return win;
+   }, { value: 0 });
+
+   // Pull the option with the highest value
+   console.log('WINNER: ', winner);
+}());
